92a767f969e2405fb22161d0e597afd32074fa97
Andre <devals2022@gmail.com>
2022-10-17 18:45:33 -0300

    atualizaprograma2

--- main.js
+++ main.js
@@ -15,5 +15,19 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
         tocaSom(idAudio);
     }
 
+  tecla.onkeydown = function(evento){
+    
+    console.log(evento.code === 'Space');
+
+    if(evento.code === 'Space'){
+     tecla.classList.add('ativa');
+    }
+  }
+
+  tecla.onkeyup = function(){
+    tecla.classList.remove('ativa');
+
+ }
+
 }
 
